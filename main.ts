e58b016b2019a42c5dd1f06b2e05a36696cbd4e2
sophia-yasyszczuk <sophia.yasyszczuk@stu.ocsb.ca>
2026-03-06 13:22:18 -0500

    Made the microbit do a spotlight

--- main.ts
+++ main.ts
@@ -1,8 +1,34 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Sophia
+ * Created on: Mar 2026
+ * This program will make a stoplight
 */
 
-basic.showString('Hello, World!')
+// variables
+let neopixelStrip: neopixel.Strip = null
+
+// cleanup
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+basic.pause(2000)
+neopixelStrip.show()
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+basic.pause(2000)
+neopixelStrip.show()
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+basic.pause(2000)
+neopixelStrip.show()
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
